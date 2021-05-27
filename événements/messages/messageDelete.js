8a5abd111fdaae58cdaf2390089237cffdf2aad1
Maxtar Gaming <gamaxtar@gmail.com>
2021-05-27 23:48:30 +0200

    ajout détection embed dans messageDelete.js

--- événements/messages/messageDelete.js
+++ événements/messages/messageDelete.js
@@ -4,14 +4,22 @@ module.exports = async (client, message) => {
 
     await Discord.Util.delayFor(900);
 
+    let babacool
+
     if (message.content.startsWith('/') || message.channel.id === '726428190024925212' || message.content.includes("Redémarrage en cours...")) return;
 
     const logchannel = message.guild.channels.cache.find(c => c.id == "726428190024925212")
 
+    if(message.embeds.length === 0){
+        babacool = message.content
+    }else{
+        babcool = 'c\'est un embed'
+    }
+
     const embed = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setDescription(`**Nouveau message supprimé !** \n **Auteur du message**: ${message.author} \n**Contenu du message**: ${message.content} \n**Salon du message**: <#${message.channel.id}>`)
+        .setDescription(`**Nouveau message supprimé !** \n **Auteur du message**: ${message.author} \n**Contenu du message**: ${babacool} \n**Salon du message**: <#${message.channel.id}>`)
         .setTimestamp()
     if (!logchannel) return;
-    logchannel.send(embed);
+    logchannel.send(embed); 
 }
